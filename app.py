e481c0ee29d0768faa1517bb5543f56d43caf928
Sukhmanjit Singh <2024tm93086@wilp.bits-pilani.ac.in>
2025-09-04 14:46:18 +0530

    apis created to add and view workouts

--- app.py
+++ app.py
@@ -1,8 +1,37 @@
-import tkinter as tk
-from tkinter import messagebox
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+workouts = []
+
+
+@app.route("/add-workout", methods=['POST'])
+def add_workout():
+    try:
+        data = request.get_json()
+        workout = data["workout"]
+        duration_str = data["duration"]
+        try:
+            duration = int(duration_str)
+            workouts.append({"workout": workout, "duration": duration})
+            return {
+                "msg": f"{workout} added successfully!"
+            }
+        except ValueError:
+            return {
+                "error": "Duration must be a number."
+            }, 400
+    except:
+        return {
+            "error": "Please enter both workout and duration."
+        }, 400
+
+
+@app.route("/view-workouts", methods=['GET'])
+def view_workouts():
+    return {
+        "workouts": workouts
+    }
 
 
 @app.route("/")
